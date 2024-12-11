f4307c3d5867587f10412cb9007c41bc3a5ad53e
Karen Andrea Sanabria González <168386906+KarenY93@users.noreply.github.com>
2024-12-11 09:29:35 -0500

    Update AccountModel.hpp

--- include/models/AccountModel.hpp
+++ include/models/AccountModel.hpp
@@ -9,7 +9,7 @@ private:
   std::string accountNumber;
   std::string status;
 public:
-  AccountModel(std::string owner,float balance);
+  AccountModel(std::string owner,std::string accountNumber, std::string status, float balance);
   float GetBalance();
   std::string GetOwner();
   std::string GetAccountNumber();
